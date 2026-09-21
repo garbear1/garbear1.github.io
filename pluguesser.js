95fd2351cc568798867355b75cfd4212fbe7960b
Shane Goodrick <shanegoodrick@gmail.com>
2026-09-21 12:06:49 -0700

    Minor pluguesser improvements

--- pluguesser.js
+++ pluguesser.js
@@ -1,39 +1,44 @@
-const product_table = {
-    "Asparagus (kg)": [4080],
+// please follow this new format when adding products
+const prod_list = [
+    ["Asparagus (kg)",                          [4080]],
 
-    "Bananas (kg)": [4011],
-    "Plantains [Ripe] (kg)": [4235],
+    ["Bananas (kg)",                            [4011]],
+    ["Plantains [Ripe] (kg)",                   [4235]],
 
-    "Broccoli (ea)": [4060],
-    "Broccoli [Crowns] (kg)": [3082],
+    ["Broccoli (ea)",                           [4060]],
+    ["Broccoli [Crowns] (kg)",                  [3082]],
     
-    "Corn [Bicolor] (ea)": [407],
+    ["Corn [Bicolor] (ea)",                     [407]],
     
-    "Eddoes (kg)": [431],
+    ["Eddoes (kg)",                             [431]],
     
-    "Grapes [Green Seedless] (kg)": [4022],
-    "Grapes [Red Seedless] (kg)": [4023, 4635],
+    ["Grapes [Green Seedless] (kg)",            [4022]],
+    ["Grapes [Red Seedless] (kg)",              [4023, 4635]],
     
-    "Watermelon [Red Seedless] (ea)": [4032],
-    // mini seedless
-}
-const product_names = Object.keys(product_table);
-
-let current_product;
-const product_heading = document.getElementById("product");
+    ["Watermelon [Red Seedless] (ea)",          [4032]],
+    ["Watermelon [Mini Seedless] (ea)",         [3421]],
+];
+let current_prod;
 
 // generates a random integer between 0 and `x`, exclusive
-function randn(x) {
-    return Math.floor(x * Math.random());
+const rand = x => Math.floor(x * Math.random());
+
+function set_random_prod() {
+    current_prod = prod_list[rand(prod_list.length)];
+}
+function get_prod_name() {
+    return current_prod[0];
+}
+function prod_has_plu(plu) {
+    return current_prod[1].includes(plu);
 }
 
-function set_random_product() {
-    current_product = product_names[randn(product_names.length)];
-    
-    product_heading.innerHTML = current_product;
-}
+const guess_in = document.getElementById("guess");
+const prod_out = document.getElementById("product");
+const status_out = document.getElementById("status");
 
-set_random_product();
+set_random_prod();
+prod_out.innerHTML = get_prod_name();
 
 document.getElementById("guess-form").addEventListener("submit", e => {
     e.preventDefault();
@@ -45,16 +50,13 @@ document.getElementById("guess-form").addEventListener("submit", e => {
         if (pair[0] === "guess") guess = +pair[1];
     }
 
-    let correct = false;
-    for (let plu of product_table[current_product]) {
-        if (guess === plu) correct = true;
+    if (prod_has_plu(guess)) {
+        status_out.style = "color:green";
+        status_out.innerHTML = "You win! :)";
+    } else {
+        status_out.style = "color:red";
+        status_out.innerHTML = "<p>You lose! :(";
     }
 
-    if (correct) {
-        alert("Hurrah!!!!!");
-    } else {
-        alert("You Lose! BOOOOOOO!!!!");
-    }
-    document.getElementById("guess").value = null;
-    window.location.reload();
+    setTimeout(() => window.location.reload(), 2000);
 });
